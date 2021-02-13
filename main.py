d6b30ed7d22a280687e27b2486a5dea9ee1132ee
kelvin5hart <atemiehartkelvin@rocketmail.com>
2021-02-13 10:10:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+import art
+
+print(art.logo)
+entries = {}
+continueBid = "yes"
+
+while continueBid == "yes":
+  name = input("What is you name? \n")
+  bid = int(input("What's your bid? \n$"))
+  optionNext = input("Are there other bidders? Type 'yes' or 'no' \n").lower()
+  entries[name] = bid
+  clear()
+  print(entries)
+  if optionNext == "no":
+    continueBid = "no"
+
+score = 0
+for person in entries:
+  if entries[person] > score:
+    score = entries[person]
+    nameOfPerson = person
+
+print(f"Highest bidder is {nameOfPerson} with a bid of ${score}")
